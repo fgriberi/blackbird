6cbb5caf03a7a7a9c94b63088d36edcb17296275
Franco Riberi <fgriberi@gmail.com>
2018-06-24 12:01:41 -0300

    [refactor-exchanges-interface] update Gemini and GDAX exhanges

--- src/exchanges/gemini.h
+++ src/exchanges/gemini.h
@@ -1,28 +1,47 @@
+/*
+ * @file     gemini.h
+ * @brief    Provides the interface to for exchanging using Gemini.
+ */
+
 #ifndef GEMINI_H
 #define GEMINI_H
 
-#include "quote_t.h"
 #include <string>
+#include <sstream>
+#include "iExchange.h"
+#include "quote_t.h"
+#include "utils/restapi.h"
+#include "unique_json.hpp"
 
-struct json_t;
-struct Parameters;
+namespace NSExchange
+{
 
-namespace Gemini {
+/**
+ * @brief Gemini is an implementation of IExchange interface. https://gemini.com/
+ */
+class Gemini : public IExchange
+{
+	virtual ~Gemini() = default;
 
-quote_t getQuote(Parameters &params, std::string pair);
+	quote_t getQuote(Parameters &params, std::string pair) override final;
 
-double getAvail(Parameters& params, std::string currency);
+	double getAvail(Parameters& params, std::string currency) override final;
 
-std::string sendLongOrder(Parameters& params, std::string direction, double quantity, double price, std::string pair);
+	std::string sendLongOrder(Parameters& params, std::string direction, double quantity, double price, std::string pair) override final;
 
-bool isOrderComplete(Parameters& params, std::string orderId);
+	std::string sendShortOrder(Parameters &params, std::string direction, double quantity, double price, std::string pair) override final;
 
-double getActivePos(Parameters& params, std::string currency);
+	bool isOrderComplete(Parameters& params, std::string orderId) override final;
 
-double getLimitPrice(Parameters& params, double volume, bool isBid, std::string pair);
+	double getActivePos(Parameters& params, std::string currency) override final;
 
-json_t* authRequest(Parameters& params, std::string url, std::string request, std::string options);
+	double getLimitPrice(Parameters& params, double volume, bool isBid, std::string pair) override final;
 
-}
+	json_t* authRequest(Parameters& params, std::string url, std::string request, std::string options);
 
-#endif
+	static RestApi& queryHandle(Parameters &params);
+};
+
+} //namespace NSExchange
+#endif /* GEMINI_H */
+
